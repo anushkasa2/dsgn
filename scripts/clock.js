0b96b9335a9c721f071259edc9affdfe01c66688
anushkasa2 <anushkaagarwal521@gmail.com>
2023-05-08 14:26:23 -0400

    final project

--- scripts/clock.js
+++ scripts/clock.js
@@ -6,9 +6,11 @@ function showTime() {
     let min = time.getMinutes();
     let sec = time.getSeconds();
     am_pm = "AM";
-  
+   
     if (hour > 12) {
         hour -= 12;
+        am_pm = "PM";
+    } else if (hour == 12) {
         am_pm = "PM";
     }
     if (hour == 0) {
